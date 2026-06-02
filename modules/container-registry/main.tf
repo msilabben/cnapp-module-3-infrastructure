b6624a230b47b0758dd60d70e7eb65d537a0e950
Knutsaether <59619961+Knutsaether@users.noreply.github.com>
2026-06-02 08:47:22 +0000

    fix: terraform format

--- modules/container-registry/main.tf
+++ modules/container-registry/main.tf
@@ -7,12 +7,12 @@ resource "random_string" "acr_name" {
 }
 
 resource "azurerm_container_registry" "this" {
-  name                          = "${var.name_prefix}${random_string.acr_name.result}acr"
+  name = "${var.name_prefix}${random_string.acr_name.result}acr"
   #checkov:skip=CKV_AZURE_139:`public_network_access_enabled` can only be disabled for a Premium Sku
-  resource_group_name           = var.resource_group_name
-  location                      = var.location
-  sku                           = var.sku
-  admin_enabled                 = var.acr_admin_enabled
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  sku                 = var.sku
+  admin_enabled       = var.acr_admin_enabled
 
   tags = var.tags
 }
