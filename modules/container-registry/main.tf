2b2fe4666b6edc1e4d2b7ff3c5524500a822ff58
Knutsaether <59619961+Knutsaether@users.noreply.github.com>
2026-06-02 08:38:42 +0000

    fix: fjernet endring som krever premium sku

--- modules/container-registry/main.tf
+++ modules/container-registry/main.tf
@@ -8,11 +8,11 @@ resource "random_string" "acr_name" {
 
 resource "azurerm_container_registry" "this" {
   name                          = "${var.name_prefix}${random_string.acr_name.result}acr"
+  #checkov:skip=CKV_AZURE_139:`public_network_access_enabled` can only be disabled for a Premium Sku
   resource_group_name           = var.resource_group_name
   location                      = var.location
   sku                           = var.sku
   admin_enabled                 = var.acr_admin_enabled
-  public_network_access_enabled = false
 
   tags = var.tags
 }
